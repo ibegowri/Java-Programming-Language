c869c90ab9e6e5de4330dfc2dc1020650053cb35
I AM GOWRI <I AM GOWRI@192.168.1.3>
2024-09-20 15:59:31 +0530

    printed text on the console

---
+++ Java_Programming_Language/src/java_print_output/Java_Print_Text.java
@@ -0,0 +1,25 @@
+package java_print_output;
+
+public class Java_Print_Text {
+
+	public static void main(String[] args) {
+
+		/**
+		 * println() method print text & add new line
+		 */
+
+		System.out.println("Hi!");
+		System.out.println("I am learning java programming language!");
+
+		// error
+		// System.out.println(I am learning java programming language!);
+
+		/**
+		 * print() method print text & doesn't add new line
+		 */
+		System.out.print("I am happy to learn. ");
+		System.out.print("I am curious to learn");
+
+	}
+
+}
